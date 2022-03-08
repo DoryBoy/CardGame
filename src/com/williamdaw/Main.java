1cbd3a0ff95aedb3598876ed714b68cfc2828a76
DoryBoy <92251353+DoryBoy@users.noreply.github.com>
2022-03-08 21:35:42 +0000

    fixed scoring

--- src/com/williamdaw/Main.java
+++ src/com/williamdaw/Main.java
@@ -5,13 +5,40 @@ import java.util.*;
 
 
 public class Main {
+    private static ArrayList<ArrayList<String>> playerNameList = new ArrayList<>();
+    private static ArrayList<Player> playerList = new ArrayList<>();
+    public static void winningCalulator(Player dealer,Deck cards){
+        ArrayList<Integer> scores = new ArrayList<>();
+        for (int i = 0; i < playerList.size(); i++) {
+           Player player = playerList.get(i);
+            System.out.println(player.get_name() + " your score is " + player.get_score());
+            scores.add(player.get_score());
+        }
+        int n = scores.size();
+        //iterate over the array comparing adjacent elements
+        int temp = 0;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(scores.get((j-1)) > scores.get(j)){
+                    //swap elements
+                    temp = scores.get((j-1));
+                    scores.set((j-1), scores.get(j));
+                    scores.set(j,temp);
+//                    scores.get(j-1) = scores.get(j);
+//                    scores.get(j) = temp;
+                }
 
+            }
+        }
+        dealer.set_hand(cards.deal(2));
+        System.out.println(dealer.get_name() + "'s cards are: ");
+        dealer.CalculateScore();
+    }
     public static void main(String[] args) {
         //Initialisation.
         Deck cards = new Deck();
         String playerName;
-        ArrayList<ArrayList<String>> playerNameList = new ArrayList<>();
-        ArrayList<Player> playerList = new ArrayList<>();
+
 
         Scanner input = new Scanner(System.in);
         for (int i = 0; i < 2; i++) {
@@ -22,7 +49,9 @@ public class Main {
 
         }
         System.out.println(playerNameList);
-        //Create player objects
+        for (int i = 0; i < 2; i++) {
+
+        }
         Player player1 = new Player((playerNameList.get(0).get(0)));
         Player player2 = new Player(playerNameList.get(1).get(0));
         playerList.add(player1);
@@ -36,32 +65,34 @@ public class Main {
             player.set_hand(cards.deal(2));
             System.out.println(player.get_name() + " your cards are: ");
             player.CalculateScore();
-
             playerNameList.add(new ArrayList<String>());
             playerNameList.get(i).add(Integer.toString(player.get_score()));
-        boolean play = true;
-        while (play) {
-            for (int j = 0; j < playerList.size(); j++) {
-
-            }
             System.out.println("What would you like to do (S)tick or (D)eal another card? ");
             String player_choice = input.nextLine().toLowerCase(Locale.ROOT);
-            if (player_choice.equals("s")) {
-                play = false;
-                System.out.println(player.get_name() + " your score is " + player.get_score());
-                //Deal hand of 2 cards to dealer and show cards.
-                dealer.set_hand(cards.deal(2));
-                System.out.println(dealer.get_name() + "'s cards are: ");
-                dealer.CalculateScore();
+            boolean play = true;
+            while (play){
+                if (player_choice.equals("s")) {
+                    break;
+
+                } else if (player_choice.equals("d")) {
+                    player.set_hand(cards.deal(1));
+                    player.CalculateScore();
+                    playerNameList.get(i).add(Integer.toString(player.get_score()));
+                }
+
+            }
+            winningCalulator(dealer,cards);
+
+
+            //Deal hand of 2 cards to dealer and show cards.
+           {
+
+
                 if (player.get_score() > dealer.get_score()) {
                     System.out.println(player.get_name() + "wins! with a score of " + player.get_score() + " dealer scored: " + dealer.get_score());
                 } else {
                     System.out.println("Dealer wins, you didn't beat the dealer, dealer scored: " + dealer.get_score() + " player scored: " + player.get_score());
                 }
-            } else if (player_choice.equals("d")) {
-                player.set_hand(cards.deal(1));
-                player.CalculateScore();
-            }
             }
         }
 
